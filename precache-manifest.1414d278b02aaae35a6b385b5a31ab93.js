cb4e14918de80532fbe8ba2d63dcff7e17887dfe
Tyron keet <tyron.keet.01@gmail.com>
2020-08-21 13:31:44 +0200

    Updates

---
+++ precache-manifest.1414d278b02aaae35a6b385b5a31ab93.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a967f7d09296218a587ef9f177ceb7e",
+    "url": "/New-stuff/index.html"
+  },
+  {
+    "revision": "dc406e31914b80a0b56e",
+    "url": "/New-stuff/static/css/2.d42dd038.chunk.css"
+  },
+  {
+    "revision": "f77e491117382f86687b",
+    "url": "/New-stuff/static/css/main.c98c3b90.chunk.css"
+  },
+  {
+    "revision": "dc406e31914b80a0b56e",
+    "url": "/New-stuff/static/js/2.efea857a.chunk.js"
+  },
+  {
+    "revision": "4e9fc953b56f50b94807904cfdad3110",
+    "url": "/New-stuff/static/js/2.efea857a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77e491117382f86687b",
+    "url": "/New-stuff/static/js/main.804119f8.chunk.js"
+  },
+  {
+    "revision": "ff17485a1aec249754de",
+    "url": "/New-stuff/static/js/runtime-main.214f7f6a.js"
+  },
+  {
+    "revision": "d45822295c0a86f59503e28c09507fc4",
+    "url": "/New-stuff/static/media/About_image.d4582229.png"
+  },
+  {
+    "revision": "6a52ea8f804182699a2c6a217538a832",
+    "url": "/New-stuff/static/media/Red.6a52ea8f.jpg"
+  },
+  {
+    "revision": "169ce861959c3299c3e463eeca6d5702",
+    "url": "/New-stuff/static/media/computer-keyboards-black-white-wallpaper2.169ce861.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/New-stuff/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/New-stuff/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/New-stuff/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/New-stuff/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/New-stuff/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "96daaf6a14dc8ddd3c2517746c8790b1",
+    "url": "/New-stuff/static/media/image1.96daaf6a.png"
+  },
+  {
+    "revision": "431f0248e2d37ab0251e558828811d5f",
+    "url": "/New-stuff/static/media/image2.431f0248.png"
+  },
+  {
+    "revision": "ee08ea9019a83d868c0db7c79f060c50",
+    "url": "/New-stuff/static/media/image3.ee08ea90.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/New-stuff/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/New-stuff/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/New-stuff/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/New-stuff/static/media/ionicons.621bd386.svg"
+  }
+]);
